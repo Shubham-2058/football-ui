062dcd81c760ebbc9ac3a90cd3e9358f7d5d9019
Shubham Bhattarai <bhattaraishubham@gmail.com>
2024-06-08 09:44:02 +0545

    latest

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './App.css'; // You can create an empty CSS file or use it for global styles
+// src/index.js
 
-ReactDOM.render(
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
+
+reportWebVitals();
